c40444e4162f7f8930383c73d34378027f637464
Ivan Bielozorov <vanya2143@gmail.com>
2020-10-24 23:06:27 +0300

    Debug false

--- brieflyMain/settings.py
+++ brieflyMain/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=#-vj0o6$i&rk1lflx$q@7cdh@+1*qwv(^^+p)%su-^j&qjc76'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
