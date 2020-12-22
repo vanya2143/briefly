855fc74a8ed2ff1f0de0d75614e217037b3d27cf
Ivan Bielozorov <vanya2143@gmail.com>
2020-12-22 22:46:41 +0200

    [Fix] Dockerfile

--- brieflyMain/settings.py
+++ brieflyMain/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'yqfPpgwM5XpidF9')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+try:
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+except AttributeError:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
